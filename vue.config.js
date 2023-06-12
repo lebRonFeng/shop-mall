70bb18ef218cd5331b7ba430e1ca27c0359b2028
刘辉丰 <liu_huifeng@topsec.com.cn>
2023-06-12 11:33:42 +0800

    '提交axios封装API接口及引入进度条代码'

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
     // 关闭eslint
-    lintOnSave: false
+    lintOnSave: false,
+    // 配置代理跨域
+    devServer:{
+        proxy:{
+            "/api":{
+                target: "http://gmall-h5-api.atguigu.cn"
+                // target: 'http://39.98.123.211',
+                // pathRewrite:{'^/api':''}
+            }
+        }
+    }
 }
